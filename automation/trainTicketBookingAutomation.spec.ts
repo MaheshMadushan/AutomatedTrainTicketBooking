f55584046f6c44da434d681955a4f0cb58aa7d32
MaheshMadushan <madu140@gmail.com>
2025-11-09 09:23:22 +0530

    Add functionality

--- automation/trainTicketBookingAutomation.spec.ts
+++ automation/trainTicketBookingAutomation.spec.ts
@@ -75,9 +75,37 @@ test("Book train ticked from galle to colombo", async ({ page }) => {
   await page
     .locator('xpath=//*[@id="email_"]')
     .pressSequentially("abcd@gmail.com");
-  await page.locator('xpath=//*[@id="nicpp_"]').pressSequentially("abcd");
+  await page.locator('xpath=//*[@id="nicpp_"]').pressSequentially("999999999V");
   await page
     .locator('xpath=//*[@id="mobile_"]')
     .pressSequentially("0777777777");
+
+  // select seats
+  //*[@id="onewayseatclass"]/div/div/div[2]/div[1]
+  await page.locator('xpath=//*[@id="onewayseatclass"]/div/div/div[2]/div[1]').click();
+  
+  // t & C
+  await page.locator('xpath=//*[@id="termsAndCon"]').click();
+
+  // proceed to payment
+  await page.locator('xpath=//*[@id="exampleModalScrollable"]/div/div/div[3]/button[1]').click();
+  await page.locator('xpath=//*[@id="inlineRadioService1"]').click();
+  await page.locator('xpath=//*[@id="submitPageBtn"]').click();
+
+  // wait for navigation to complete reservation
+  await page.waitForURL("**/reservation");
+  await page.waitForURL("https://seylan.gateway.mastercard.com/checkout/pay/**");
+
+  console.log(page.url())
+  
+  // !!!!!!!!!!!!!!!WARNING: Use test cards only in testing environments!!!!!!!!!!!!!!!!!!!!!!
+  await page.locator('xpath=//*[@id="cardNumber"]').fill("5555555555554444");
+  await page.locator('xpath=//*[@id="expiryMonth"]').selectOption("09");
+  await page.locator('xpath=//*[@id="expiryYear"]').selectOption("45");
+  await page.locator('xpath=//*[@id="csc"]').fill("123");
+  await page.locator('xpath=//*[@id="cardHolderName"]').fill("ABCD ABCD");
+  await page.locator('xpath=//*[@id="button-row1"]/button[1]').click(); 
+
+  expect(page.url()).toContain("seylan.gateway.mastercard.com/checkout/pay/**");
 
 });
