101419b25676eb32dfd642c07a5d514008666c0f
MaheshMadushan <madu140@gmail.com>
2025-11-09 09:39:47 +0530

    Add Readme howto

---
+++ README.md
@@ -0,0 +1,11 @@
+# Playwright automation - train ticket booking
+
+### How to
+- Clone the repository
+    - `git clone https://github.com/MaheshMadushan/AutomatedTrainTicketBooking.git`
+- Please read the code and change nessary information that as user intended.
+    - `ctrl + f` for phrase `CHANGE as user intened` and change values accordingly.
+- Install playwright (should have nodejs installed)
+    - `npm init playwright@latest`
+- Run the playwright automation and have your ticket booked
+    - `npx playwright test --ui`
--- automation/trainTicketBookingAutomation.spec.ts
+++ automation/trainTicketBookingAutomation.spec.ts
@@ -7,7 +7,7 @@ test("Book train ticked from galle to colombo", async ({ page }) => {
   await page
     .locator('xpath=//*[@id="regular-user"]/form/div[1]/div[1]/span')
     .click();
-  await page.locator("xpath=/html/body/span/span/span[1]/input").fill("galle");
+  await page.locator("xpath=/html/body/span/span/span[1]/input").fill("galle"); // (from which station) CHANGE as user intened 
   await page.locator('xpath=//*[@id="select2-fromstation-results"]').click();
 
   // select colombo
@@ -16,14 +16,14 @@ test("Book train ticked from galle to colombo", async ({ page }) => {
     .click();
   await page
     .locator("xpath=/html/body/span/span/span[1]/input")
-    .fill("colombo fort");
+    .fill("colombo fort"); // (to which station) CHANGE as user intened 
   await page.locator('xpath=//*[@id="select2-tostation-results"]').click();
 
   // select date
-  await page.locator('xpath=//*[@id="dateOne"]').fill("2025-11-10");
+  await page.locator('xpath=//*[@id="dateOne"]').fill("2025-11-10"); // (when?) CHANGE as user intened 
 
   // num of passengers fill
-  await page.locator('xpath=//*[@id="passengers"]').fill("1");
+  await page.locator('xpath=//*[@id="passengers"]').fill("1"); // for now only support single passenger
 
   // purge recaptcha
   await page.evaluate(() => {
@@ -37,9 +37,6 @@ test("Book train ticked from galle to colombo", async ({ page }) => {
   await page
     .locator('xpath=//*[@id="regular-user"]/form/button[2]')
     .click()
-    .then(() => {
-      console.log("DONE");
-    });
 
   // wait for navigation to complete
   await page.waitForURL("**/dashboard");
@@ -48,9 +45,6 @@ test("Book train ticked from galle to colombo", async ({ page }) => {
   await page
     .locator('xpath=//*[@id="msgModal"]/div/div/div[3]/button')
     .click()
-    .then(() => {
-      console.log("DONE");
-    });
 
   // popup handling
   await page
@@ -58,9 +52,6 @@ test("Book train ticked from galle to colombo", async ({ page }) => {
       "xpath=/html/body/section[3]/div/div[1]/section/div/div/div/div[2]/div/div[2]/div/table/tbody/tr[2]"
     )
     .click()
-    .then(() => {
-      console.log("DONE");
-    });
 
   // press proceed button
   await page.locator('xpath=//*[@id="landingProceedBtn"]').click();
@@ -70,15 +61,15 @@ test("Book train ticked from galle to colombo", async ({ page }) => {
 
   // fill passenger details
   await page.locator('xpath=//*[@id="fname_"]').click();
-  await page.locator('xpath=//*[@id="fname_"]').pressSequentially("abcd");
-  await page.locator('xpath=//*[@id="lname_"]').pressSequentially("abcd");
+  await page.locator('xpath=//*[@id="fname_"]').pressSequentially("abcd"); // (first name) CHANGE as user intened 
+  await page.locator('xpath=//*[@id="lname_"]').pressSequentially("abcd"); // (last name) CHANGE as user intened 
   await page
     .locator('xpath=//*[@id="email_"]')
-    .pressSequentially("abcd@gmail.com");
-  await page.locator('xpath=//*[@id="nicpp_"]').pressSequentially("999999999V");
+    .pressSequentially("abcd@gmail.com"); // (email) CHANGE as user intened 
+  await page.locator('xpath=//*[@id="nicpp_"]').pressSequentially("999999999V"); // (nic) CHANGE as user intened 
   await page
     .locator('xpath=//*[@id="mobile_"]')
-    .pressSequentially("0777777777");
+    .pressSequentially("0777777777"); // (mobile num) CHANGE as user intened 
 
   // select seats
   //*[@id="onewayseatclass"]/div/div/div[2]/div[1]
@@ -97,15 +88,17 @@ test("Book train ticked from galle to colombo", async ({ page }) => {
   await page.waitForURL("https://seylan.gateway.mastercard.com/checkout/pay/**");
 
   console.log(page.url())
+  expect(page.url()).toContain("https://seylan.gateway.mastercard.com/checkout/pay/");
   
   // !!!!!!!!!!!!!!!WARNING: Use test cards only in testing environments!!!!!!!!!!!!!!!!!!!!!!
-  await page.locator('xpath=//*[@id="cardNumber"]').fill("5555555555554444");
-  await page.locator('xpath=//*[@id="expiryMonth"]').selectOption("09");
-  await page.locator('xpath=//*[@id="expiryYear"]').selectOption("45");
-  await page.locator('xpath=//*[@id="csc"]').fill("123");
-  await page.locator('xpath=//*[@id="cardHolderName"]').fill("ABCD ABCD");
+  await page.locator('xpath=//*[@id="cardNumber"]').fill("5555555555554444"); // (card num) CHANGE as user intened 
+  await page.locator('xpath=//*[@id="expiryMonth"]').selectOption("09"); // (expiry month) CHANGE as user intened 
+  await page.locator('xpath=//*[@id="expiryYear"]').selectOption("45");// (expiry date) CHANGE as user intened 
+  await page.locator('xpath=//*[@id="csc"]').fill("123");  // (card cvv) CHANGE as user intened 
+  await page.locator('xpath=//*[@id="cardHolderName"]').fill("ABCD ABCD"); // (name) CHANGE as user intened 
+  
   await page.locator('xpath=//*[@id="button-row1"]/button[1]').click(); 
 
-  expect(page.url()).toContain("seylan.gateway.mastercard.com/checkout/pay/**");
+  
 
 });
